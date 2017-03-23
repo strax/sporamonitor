82d1a35d46b90ef7d41022e237824eee0bd048bd
Sami Kukkonen <sami@samikukkonen.fi>
2017-03-23 11:28:05 +0200

    Change HSLLiveClient.ENDPOINT_URI to final (constant)

--- sporamonitor/src/main/java/sporamonitor/hslapi/HSLLiveClient.java
+++ sporamonitor/src/main/java/sporamonitor/hslapi/HSLLiveClient.java
@@ -3,7 +3,7 @@ package sporamonitor.hslapi;
 import java.net.URI;
 
 public class HSLLiveClient {
-    private static URI ENDPOINT_URI = URI.create("http://dev.hsl.fi/siriaccess/vm/json?operatorRef=HSL");
+    private static final URI ENDPOINT_URI = URI.create("http://dev.hsl.fi/siriaccess/vm/json?operatorRef=HSL");
 
     public HSLLiveClient() {
     }
